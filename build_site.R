edd2e7b37da3409d9ce2861fd58a7c282aa299ce
R. Jordan Crouser <jcrouser@smith.edu>
2018-01-23 17:19:08 -0500

    Completing merge

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/jcrouser/Google Drive/Teaching/Course Material/CSC220/CSC220-website")
+setwd("/Users/jcrouser/Google Drive/Teaching/Course Material/CSC250/CSC250-website")
 
 #render site. 
 rmarkdown::render_site()
