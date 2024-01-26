0b9a4b0cb7284533ed34caf9189d4b2011b1cabd
R. Jordan Crouser <jcrouser@smith.edu>
2024-01-26 11:23:42 -0500

    Added Slack

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+#setwd("/Users/jcrouser/Google Drive/Teaching/Course Material/CSC250/CSC250-website")
+
+#render site. 
+rmarkdown::render_site()
